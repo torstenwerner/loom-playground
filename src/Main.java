87b7c4a6e7eb1795d42118a8e9e956e8bd922d54
Torsten Werner <torsten.werner@westernacher.com>
2018-12-31 21:44:29 +0100

    reduce static

--- src/Main.java
+++ src/Main.java
@@ -6,26 +6,30 @@ import static java.util.stream.Collectors.toList;
 
 public class Main {
 
-    private final static int MAX_FIBERS = 64;
+    private final int MAX_FIBERS = 64;
 
     public static void main(String[] args) {
+        new Main().run();
+    }
+
+    private void run() {
         IntStream.range(0, MAX_FIBERS)
-                .mapToObj(Main::startFiber)
+                .mapToObj(this::startFiber)
                 .collect(toList())
                 .forEach(Fiber::join);
     }
 
-    private static Fiber startFiber(int id) {
+    private Fiber startFiber(int id) {
         final Fiber<Void> fiber = Fiber.schedule(() -> {
             System.out.printf("fiber %d before sleep, time %d%n", id, currentTimeMillis());
-            sleep(100 * (MAX_FIBERS - id));
+            sleep((MAX_FIBERS - id) * 100);
             System.out.printf("fiber %d after sleep, time %d%n", id, currentTimeMillis());
         });
         System.out.printf("fiber %d scheduled with name %s%n", id, fiber);
         return fiber;
     }
 
-    private static void sleep(int millis) {
+    private void sleep(int millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
